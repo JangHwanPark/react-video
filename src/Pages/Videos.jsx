b7827a75605e38ec10887bdb24115bdf682e50d9
JangHwanPark <wkd5878@gmail.com>
2024-03-03 14:20:38 +0900

    [Feat] Videos 페이지 내 인기 동영상 데이터 제목 패칭

--- src/Pages/Videos.jsx
+++ src/Pages/Videos.jsx
@@ -5,15 +5,22 @@ import useFetch from "../Hooks/useFetch";
 export default function Videos() {
     const { query } = useParams();
 
-    const { loading, error, value } = useFetch('/data/video/related.json', {}, []);
+    const { loading, error, value } = useFetch('/data/video/popular.json', {}, []);
     if (loading) return <div>로딩중</div>
     if (error) return <div>에러발생 : {error.message}</div>
-    console.log(value);
+    console.log("value : ", value);
+    console.log("value.items : ", value.items);
 
     return (
         <div>
-            <p>Videos</p>
+            <p>Page : Videos</p>
             <p>query : {query ? `${query}` : 'Not'}</p>
+
+            <br/>
+
+            {value.items.map(videoData => (
+                <div key={videoData.id}>{videoData.snippet.title}</div>
+            ))}
         </div>
     );
 }
