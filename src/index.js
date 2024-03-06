44c6069410457c2acd8872f33e47eb0a06a80da4
japark <wkd5878@gmail.com>
2024-03-06 11:13:12 +0900

    [Feat] Setup Detail Page Layout

--- src/index.js
+++ src/index.js
@@ -25,9 +25,9 @@ const router = createBrowserRouter([
 
 const root = ReactDOM.createRoot(document.getElementById('wrap'));
 root.render(
-  <React.StrictMode>
-      <RouterProvider router={router}/>
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/VideosDetail.jsx
+++ src/pages/VideosDetail.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import styles from './VideosDetail.css';
 import {useLocation, useParams} from "react-router-dom";
+import {formatAgo} from "../util/date";
 
 export default function VideosDetail() {
-    const { videoId } = useParams();
-    const { state : { videoData } } = useLocation();
-    const { title, description, tags } = videoData.snippet;
+    const {state: {videoData}} = useLocation();
+    const {title, publishedAt, description, tags} = videoData.snippet;
+    const videoLink = `https://www.youtube.com/embed/${videoData.id}`;
 
     return (
-        <main>
-            <h1>VideosDetail</h1>
-            <p>동적 라우트 값 (videoId) : {videoId}</p>
-            <div>title : {title}</div>
-            <div>description : {description}</div>
-            <div>publishedAt : publishedAt</div>
-            <div>tags : {tags}</div>
+        <main className={styles.video_container}>
+            <iframe width="640" height="360" src={videoLink} frameBorder="0"/>
+            <h1>{title}</h1>
+            <div>
+                <span>조회수 n회</span>
+                <span>{formatAgo(publishedAt, 'ko')}</span>
+            </div>
+            <p>{description}</p>
+            <span>{tags}</span>
         </main>
     );
 }
